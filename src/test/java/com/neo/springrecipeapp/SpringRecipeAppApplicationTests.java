1285e6e5b6ef895219b1b852929bbb5dafb08268
Neo <omkarkandale@gmail.com>
2020-07-11 20:18:48 +0530

    Adding controllers and domain

---
+++ src/test/java/com/neo/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
